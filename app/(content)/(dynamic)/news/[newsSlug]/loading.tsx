f7d8f35d90f65f9075702b9a174315f689a72a46
Misy <78818724+zrfli@users.noreply.github.com>
2024-07-31 20:46:10 +0300

    fix news

---
+++ app/(content)/(dynamic)/news/[newsSlug]/loading.tsx
@@ -0,0 +1,5 @@
+export default function loading() {
+  return (
+    <div className="min-h-screen bg-black text-neutral-200">Loading...</div>
+  )
+}
--- app/(content)/(dynamic)/news/[newsSlug]/page.tsx
+++ app/(content)/(dynamic)/news/[newsSlug]/page.tsx
@@ -1,11 +1,17 @@
-"use client";
-
 import { notFound } from 'next/navigation';
-import { PrismaClient } from '@prisma/client';
+//import { PrismaClient } from '@prisma/client';
 
 //const prisma = new PrismaClient();
 
-export default async function news({ params } : { params: { newsSlug : any }}) {
+export default async function news({ params } : { params: { newsSlug : number }}) {
+  const req = await fetch(`https://dummyjson.com/posts/${params.newsSlug}`, { cache: 'force-cache' });
+  
+  //if(!req.ok){ throw new Error('failed to the fetch with api!!'); }
+
+  const res = await req.json();
+
+  if (!res || !res.id) { return notFound(); }
+  
   /*const post = {
     title: params.newsSlug,
     createdAt: 1722181361,
@@ -25,16 +31,18 @@ export default async function news({ params } : { params: { newsSlug : any }}) {
 
   if (!post) { return notFound(); }
   */
+
+ 
   return  <>
             <section className="w-full h-64 bg-no-repeat bg-cover bg-center relative border-b bg-gray-300 bg-blend-multiply border-gray-700 bg-[url('/icheader-bg.jpg')]">
                 <div className="absolute top-20 left-1/2 px-4 mx-auto w-full max-w-screen-xl -translate-x-1/2 xl:top-1/2 xl:-translate-y-1/2 xl:px-0">
-                    <h1 className="mb-4 max-w-4xl text-3xl font-extrabold text-white">{params.newsSlug ?? 'Error'}</h1>
-                    <p className="font-normal text-gray-300">{params.newsSlug ?? 'Error'}</p>
+                    <h1 className="mb-4 max-w-4xl text-3xl font-extrabold text-white">{res.title ?? 'Error'}</h1>
+                    <p className="font-normal text-gray-300">{res.title ?? 'Error'}</p>
                 </div>
             </section>
             <section className="flex relative z-20 min-h-96 justify-center px-4 mx-auto max-w-screen-lg">
                 <div className="w-full py-6">
-                  <div className="space-y-4 text-gray-900 dark:text-gray-300 h-full">{params.newsSlug ?? 'Error'}</div>
+                  <div className="space-y-4 text-gray-900 dark:text-gray-300 h-full">{res.title ?? 'Error'}</div>
                 </div>
             </section>
           </>
